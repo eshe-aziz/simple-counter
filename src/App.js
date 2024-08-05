0aafb6b55532afddfc18a53797369cf3c3d6726b
eshe-aziz <eshehaledhaziz@gmail.com>
2024-08-05 11:47:11 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
